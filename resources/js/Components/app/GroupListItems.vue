88c1b431eefd894d1cdd86d59950edae3ed213df
sangla3 <lavansang1264@gmail.com>
2024-04-17 23:08:19 +0700

    create home page ui

---
+++ resources/js/Components/app/GroupListItems.vue
@@ -0,0 +1,72 @@
+<script setup>
+import GroupItem from "@/Components/app/GroupItem.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { ref } from "vue";
+
+const searchKeyword = ref('');
+</script>
+
+<template>
+    <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4"/>
+    <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
+        <div v-if="false" class="text-gray-400 flex flex-center p-3 justify-center">
+            You are not joined to any groups
+        </div>
+        <div v-else>
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Vue.js Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Vue.js Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Vue.js Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Vue.js Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+            <GroupItem 
+                image="https://picsum.photos/100"
+                title="Vue.js Developers"
+                description="Lorem ipsum dolor sit. Dolore ad, molestiae incidunt pariatur recusandae illo beatae soluta!"
+            />
+        </div>
+    </div>    
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/PostList.vue
@@ -0,0 +1,77 @@
+<script setup>
+import PostItem from '@/Components/app/PostItem.vue';
+const post1 = {
+    user: {
+        id: 1,
+        avatar: 'https://randomuser.me/api/portraits/men/60.jpg',
+        name: 'Su La'
+    },
+    group: null,
+    attachments: [
+        {
+            id: 1,
+            name: 'test.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png'
+        },
+        {
+            id: 2,
+            name: 'test2.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png'
+        },
+        {
+            id: 3,
+            name: 'MyDocument.docx',
+            url: '#',
+            mime: 'application/msword'
+        }
+    ],
+    body: `<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Accusamus dolorum architecto deleniti non molestiae quam, ad, 
+        nisi fugiat consequatur vero iusto commodi magnam veniam similique 
+        maxime totam, repellendus maiores voluptatem.</p>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Accusamus dolorum architecto deleniti non molestiae quam, ad, 
+        nisi fugiat consequatur vero iusto commodi magnam veniam similique 
+        maxime totam, repellendus maiores voluptatem.</p>`,
+    created_at: '2024-04-14 15:54'
+}
+
+const post2 = {
+    user: {
+        id: 2,
+        avatar: 'https://randomuser.me/api/portraits/men/61.jpg',
+        name: 'Sang La'
+    },
+    group: {
+        id: 1,
+        name: 'Laravel Develop'
+    },
+    
+    body: `<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Accusamus dolorum architecto deleniti non molestiae quam, ad, 
+        nisi fugiat consequatur vero iusto commodi magnam veniam similique 
+        maxime totam, repellendus maiores voluptatem.</p>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Accusamus dolorum architecto deleniti non molestiae quam, ad, 
+        nisi fugiat consequatur vero iusto commodi magnam veniam similique 
+        maxime totam, repellendus maiores voluptatem.</p>`,
+    created_at: '2024-04-14 15:54'
+}
+
+</script>
+
+<template>
+    <div class="overflow-auto">
+        <PostItem :post="post1"/>
+
+        <PostItem :post="post2"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
